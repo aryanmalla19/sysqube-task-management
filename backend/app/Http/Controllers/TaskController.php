f511a3c8f866dee8255624c6fd28a8a50db9b5f4
Aryan Malla <aryanmalla19@gmail.com>
2025-05-29 15:37:29 +0545

    added controllers and custom request response

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return TaskResource::collection($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $data = $request->validated();
+        Task::create($data);
+
+        return response()->json([
+            'message' => 'Successfully created new task',
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $data = $request->validated();
+        $task->update($data);
+
+        return response()->json([
+            'message' => 'Successfully updated task',
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json([
+            'message' => 'Successfully deleted task',
+        ], 200);
+    }
+}
